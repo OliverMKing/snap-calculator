2f7d5012829a3f252541bb740234641387eca7ba
Oliver King <olivermerkleyking@gmail.com>
2023-01-30 20:05:44 -0500

    start homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="w-full container mx-auto">
+      <div className="container pt-6 xl:pt-8 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <div className="flex flex-col w-full lg:w-2/5 justify-center lg:items-start overflow-y-hidden">
+          <h1 className="my-4 text-4xl md:text-6xl font-bold leading-tight text-gray-800 leading-tight text-center lg:text-left">
+            Marvel Snap Calculator
+          </h1>
+          <p className="leading-normal text-gray-800 md:text-4xl text-2xl mb-8 text-center lg:text-left">
+            Make every card count for more
+          </p>
+        </div>
+      </div>
 
-        <h1 className="text-3xl font-bold underline">Hello tailwind world!</h1>
-      </header>
+      <div className="container bg-purple-600 sm:rounded-md py-8 sm:mb-8 text-center">
+        <h2 className="break-normal font-normal text-white text-4xl">
+          Enter your deck code
+        </h2>
+        <div className="mx-3 text-center pt-4">
+          <div className="max-w-sm mx-auto p-1 bg-white rounded shadow flex items-center">
+            <input
+              type="text"
+              className="flex-auto appearance-none p-3 text-gray-600"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

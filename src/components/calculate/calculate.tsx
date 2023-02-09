d317fd20cd918f40a0bcc404dfd7d82301d36751
Oliver King <olivermerkleyking@gmail.com>
2023-02-08 20:42:11 -0500

    add and bar

--- src/components/calculate/calculate.tsx
+++ src/components/calculate/calculate.tsx
@@ -35,55 +35,65 @@ const CalculateComponent: React.FC<{cards: Set<string>}> = ({cards}) => {
 
   return (
     <div>
-      <h2 className="text-2xl py-4 pl-2 md:text-4xl">
+      <h2 className="text-2xl py-4 pb-8 pl-2 md:text-4xl">
         Tell us what you want to calculate
       </h2>
 
       <div className="flex flex-col">
         {requirements.map((requirement, index) => {
           return (
-            <div
-              key={index}
-              className="pt-4 pl-2 flex flex-row gap-2 items-center"
-            >
-              <span className="text-xl">Draw</span>
-              <select
-                value={requirement.card}
-                onChange={(e) => handleRequirementsCardChange(e, index)}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            <React.Fragment>
+              <div
+                key={index}
+                className="pt-1 pl-2 flex flex-row gap-2 items-center"
               >
-                <option value={blank} disabled />
-                {Array.from(cards.keys())
-                  .sort()
-                  .map((card) => (
-                    <option key={card} value={card}>
-                      {card}
-                    </option>
-                  ))}
-              </select>
-              <span className="text-xl">by turn</span>
-              <select
-                value={requirement.byTurn}
-                onChange={(e) => handleRequirementsTurnChange(e, index)}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option value={blank} disabled />
-                {range(MIN_TURN, MAX_TURN)
-                  .map(String)
-                  .map((turn) => (
-                    <option key={turn} value={turn}>
-                      {turn}
-                    </option>
-                  ))}
-              </select>
-            </div>
+                <span className="text-xl">Draw</span>
+                <select
+                  value={requirement.card}
+                  onChange={(e) => handleRequirementsCardChange(e, index)}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option value={blank} disabled />
+                  {Array.from(cards.keys())
+                    .sort()
+                    .map((card) => (
+                      <option key={card} value={card}>
+                        {card}
+                      </option>
+                    ))}
+                </select>
+                <span className="text-xl">by turn</span>
+                <select
+                  value={requirement.byTurn}
+                  onChange={(e) => handleRequirementsTurnChange(e, index)}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option value={blank} disabled />
+                  {range(MIN_TURN, MAX_TURN)
+                    .map(String)
+                    .map((turn) => (
+                      <option key={turn} value={turn}>
+                        {turn}
+                      </option>
+                    ))}
+                </select>
+              </div>
+
+              {requirements.length !== index + 1 && (
+                <div className="pl-2 inline-flex flex-row items-center">
+                  <hr className="w-32 h-px my-8 bg-gray-200 border-0" />
+                  <span className="px-4">and</span>
+                  <hr className="w-32 h-px my-8 bg-gray-200 border-0" />
+                </div>
+              )}
+            </React.Fragment>
           )
         })}
       </div>
 
       <button
         onClick={handleAddCondition}
-        className="mt-8 ml-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+        className="my-8 ml-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
       >
         Add condition
       </button>
